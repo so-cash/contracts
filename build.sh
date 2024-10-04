febdf37ddbb995dc549bde66fba4f84a82a8e031
Bertrand Juglas <b@juglas.name>
2024-10-05 00:28:59 +0200

    use EVM version London

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-# Do not use version 0.8.20 for the moment because Kerleano and PocrNet do not support the Shanghai fork (EIP-3855)
+# Compile for EVM version London for the moment because Kerleano and PocrNet do not support the Shanghai fork (EIP-3855)
 SOLC=`(which solc>/dev/null && which solc) || echo /usr/local/bin/solc-v0.8.18`
 if [ ! -x "$SOLC" ]
 then
@@ -6,7 +6,7 @@ then
 fi
 echo "Using solc as \"$SOLC\""
 $SOLC --version
-$SOLC -o build --optimize --combined-json abi,bin,bin-runtime --overwrite --base-path . --include-path ./node_modules src/*/*.sol
+$SOLC --evm-version london -o build --optimize --combined-json abi,bin,bin-runtime --overwrite --base-path . --include-path ./node_modules src/*/*.sol
 if [ $? -eq 0 ]
 then
   echo "- Create index.js and index.d.ts files"
