8bfcad84c3436b39606ce9cebadb7c1ae9c336f1
Guenole de Cadoudal <guenoledc@yahoo.fr>
2025-02-23 13:21:33 +0100

    fix copy location error

--- sc-diamond/src/src/initializable/IInitializable.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// FeverTokens Contracts v1.0.0
-
-pragma solidity ^0.8.17;
-
-import { IInitializableInternal } from "./IInitializableInternal.sol";
-
-interface IInitializable is IInitializableInternal {}
--- sc-diamond/src/src/initializable/Initializable.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-// FeverTokens Contracts v1.0.0
-
-pragma solidity ^0.8.17;
-
-import { IInitializable } from "./IInitializable.sol";
-import { InitializableInternal } from "./InitializableInternal.sol";
-
-abstract contract Initializable is IInitializable, InitializableInternal {}
